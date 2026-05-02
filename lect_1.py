baa9faad7a590d28f196c20aae607cce71f88f44
Connect2waqas <waqasnadan972@gmail.com>
2026-05-02 16:38:21 +0500

    Refactor string handling comments and remove unnecessary whitespace

--- lect_1.py
+++ lect_1.py
@@ -8,16 +8,14 @@ name = "waqas khan"
 print(name)
 
 "Input() : through input we can get any string from the user."
-
 name = input("Enter your name: ")
 print(name)
 
 """properties:
              String are immutable.
              string cannot be modified inplace.
-             
 2 Method of Encoding in python:
                               => ASCII:-  128 (American standard code for information interchange) it can only encode 128 characters
                               => Unicode:- 143,859 (sub super set of ASCII): it can encode 143,859 character (emojies all things).
-                              
+
                               """
