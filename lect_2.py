4eaac586d0ef8bc297f665e47451ec8a79255224
Connect2waqas <waqasnadan972@gmail.com>
2026-05-02 16:50:19 +0500

    Add documentation for string functions: ord() and char()

---
+++ lect_2.py
@@ -0,0 +1,13 @@
+"""
+TWO Most Important Function of String:
+                                     1) ord()
+                                     2) char()
+                                     ord():
+                                          this is a built-in function which return an integer representing.
+                                          unicode for the given sting. 'a' --> U+0061 --> 97 --> 01100001.
+                                                    for given integer its unicode integer value then its binary value.
+                                        Syntax:
+                                               ord(argument--> should be character and only one argument.)
+                                               order("H")
+                                               """
+
