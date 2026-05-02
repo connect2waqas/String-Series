d4b9490e360ac8175ddd4620288efa6e3604739d
Connect2waqas <waqasnadan972@gmail.com>
2026-05-02 16:36:14 +0500

    Add initial implementation of string handling and encoding concepts

---
+++ lect_1.py
@@ -0,0 +1,23 @@
+"""String: A sequence of unicode character: Becuase the computer under binary numbers only.
+String is a sequence of character.
+Through encoding python convert the character to binary 
+Through decoding python convert the binary to character for showing results"""
+
+
+name = "waqas khan"
+print(name)
+
+"Input() : through input we can get any string from the user."
+
+name = input("Enter your name: ")
+print(name)
+
+"""properties:
+             String are immutable.
+             string cannot be modified inplace.
+             
+2 Method of Encoding in python:
+                              => ASCII:-  128 (American standard code for information interchange) it can only encode 128 characters
+                              => Unicode:- 143,859 (sub super set of ASCII): it can encode 143,859 character (emojies all things).
+                              
+                              """
